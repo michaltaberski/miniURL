1a28b4ac33afa4b82cd69978005e286e21233025
xTrol <xtrol87@gmail.com>
2010-07-15 11:51:07 +0200

    miniURL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '8d54344a1c156dbbc66505e4d770adb632086594286838a787b3ae7387462330e0be10d271a061ecdfea12a4ec8a901e4b82ffe1de03726a068efad1ac15a138'
